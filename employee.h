e3e4fe9370032917caa6825865de33b72794df85
nada <nada.latrach@esprit.tn>
2023-03-06 17:06:41 +0100

    employee.h is modified

--- employee.h
+++ employee.h
@@ -1,35 +1,57 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 #include <QString>
-
+#include <QSqlQuery>
+#include <QSqlQueryModel>
 class Employee
 {
 public:
+    //constructors
     Employee();
-    void setId(int ID);
+   Employee( QString ID,QString  mail,QString fname ,int Phone, QString lname, float salary, QString idstat, QString role);
+
+    //setters
+    void setId(QString ID);
      void setFname(QString fname);
      void setLname(QString lname);
      void setMail(QString mail);
      void setPhone(int Phone );
      void setRole(QString role);
      void setSalary(float salary);
-     int getId();
+      void setidstat(QString idstat);
+
+     //getters
+      QString  getId();
      QString getFname();
      QString getLname();
      QString getMail();
      int  getPhone();
      QString getRole();
      float getSalary();
+   QString  getidstat();
 
+     //CRUD
+     bool addEmp();
+     QSqlQueryModel *view();
+     bool deleteEmp(QString ID);
+      bool modify(QString IDd,QString maill,QString fnamee,int phone,QString lnamee,float salar,QString idsta,QString r);
+     QSqlQueryModel * sort_name();
+     QSqlQueryModel * sort_ID();
+     QSqlQueryModel * sort_Role();
+     QSqlQueryModel* searchname(QString na);
+     QSqlQueryModel* searchID(QString id);
+     QSqlQueryModel* searchRole(QString r);
+      void  statistics(QVector<double>* ticks,QVector<QString> *labels);
 
 private :
-    int ID;
+   QString   ID;
    QString  fname;
    QString  lname;
    QString  mail;
    int  Phone  ;
-  QString   role;
-  float  salary;
+   QString   role;
+   float  salary;
+   QString idstat;
 
 
 };

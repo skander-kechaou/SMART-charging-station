6c5445c59337204b5b1eff6987a726dea45fa242
skander-kechaou <skander.kechaou@esprit.tn>
2023-03-07 14:42:27 +0100

    2nd commit from Client on mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "client.h"
 
 #include <QMainWindow>
 
@@ -17,8 +18,14 @@ public:
 
 private slots:
     void on_addButton_clicked();
+    void on_deleteButton_clicked();
+
+    void on_tableView_clicked(const QModelIndex &index);
+
+    void on_EditButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    client Etmp;
 };
 #endif // MAINWINDOW_H

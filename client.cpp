13861b22fede3c1d9ad7c7e40de45f45f4ff0781
skander-kechaou <skander.kechaou@esprit.tn>
2023-03-07 14:41:28 +0100

    2nd commit from Client on client.cpp

--- client.cpp
+++ client.cpp
@@ -2,24 +2,96 @@
 
 client::client()
 {
-NIC="";
-fnameC="";
-lnameC="";
-mailC="";
-phoneC="";
-credit="";
+    NIC="";
+    fname="";
+    lname="";
+    mail="";
+    phoneC="";
+    credit="";
 }
+
+client::client(QString NIC, QString f, QString l, QString m, QString p, QString c)
+{
+this->NIC=NIC;
+fname=f;
+lname=l;
+mail=m;
+phoneC=p;
+credit=c;
+}
+
+bool client::Create()
+{
+    QSqlQuery query;
+
+
+        //prepare() takes the query as a parameter to prepare it for execution
+        query.prepare("insert into client (NIC, fname, lname, phoneC, mail, credit)" "values (:NIC, :fname, :lname, :phoneC, :mail, :credit)");
+
+        // Creating variables
+        query.bindValue(":NIC",NIC);
+        query.bindValue(":fname",fname);
+        query.bindValue(":lname",lname);
+        query.bindValue(":phoneC",phoneC);
+        query.bindValue(":mail",mail);
+        query.bindValue(":credit", credit);
+
+        return query.exec(); //exec() send request to be executed
+}
+
+QSqlQueryModel * client::Read()
+{
+    QSqlQueryModel * model=new QSqlQueryModel();
+
+        model->setQuery("select * from client");
+        model->setHeaderData(0,Qt::Horizontal,QObject::tr("NIC"));
+        model->setHeaderData(1,Qt::Horizontal,QObject::tr("First Name"));
+        model->setHeaderData(2,Qt::Horizontal,QObject::tr("Last Name"));
+        model->setHeaderData(3,Qt::Horizontal,QObject::tr("Phone"));
+        model->setHeaderData(4,Qt::Horizontal,QObject::tr("E-mail"));
+        model->setHeaderData(5,Qt::Horizontal,QObject::tr("Credit"));
+
+        return  model;
+}
+
+bool client::Delete(QString NIC)
+{
+    QSqlQuery query;
+
+        query.prepare("Delete from client where NIC= :NIC");
+
+        query.bindValue(":NIC",NIC);
+
+        return  query.exec();
+}
+
+bool client::update()
+{
+    QSqlQuery query;
+        query.prepare ("update client SET NIC = :NIC, fname = :fname, lname = :lname, phoneC = :phoneC, mail = :mail, credit = :credit" );
+
+        // Creating variables
+        query.bindValue(":NIC",NIC);
+        query.bindValue(":fname",fname);
+        query.bindValue(":lname",lname);
+        query.bindValue(":phoneC",phoneC);
+        query.bindValue(":mail",mail);
+        query.bindValue(":credit", credit);
+
+       return query.exec();
+}
+
 void client::setNIC(QString c){
     NIC=c;
 }
 void client::setFirstName(QString fn){
-    fnameC=fn;
+    fname=fn;
 }
 void client::setLastName(QString ln){
-    lnameC=ln;
+    lname=ln;
 }
 void client::setEmail(QString e){
-    mailC=e;
+    mail=e;
 }
 void client::setPhone(QString p){
     phoneC=p;
@@ -31,13 +103,13 @@ QString client::getNIC(){
     return NIC;
 }
 QString client::getFirstName(){
-    return fnameC;
+    return fname;
 }
 QString client::getLastName(){
-    return lnameC;
+    return lname;
 }
 QString client::getEmail(){
-    return mailC;
+    return mail;
 }
 QString client::getPhone(){
     return phoneC;
@@ -45,5 +117,3 @@ QString client::getPhone(){
 QString client::getCredit(){
     return credit;
 }
-
-

4b5e2a3c114dfdc43e87d7bf746635dbb607be3c
nada <nada.latrach@esprit.tn>
2023-03-10 23:15:08 +0100

    login.h has been added

--- login.h
+++ login.h
@@ -1,7 +1,7 @@
 #ifndef LOGIN_H
 #define LOGIN_H
-
 #include <QMainWindow>
+#include "employee.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class Login; }
 QT_END_NAMESPACE
@@ -17,7 +17,15 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_login_clicked();
+
+    void on_label_2_linkActivated(const QString &link);
+
 private:
     Ui::Login *ui;
+       Login *l;
+
 };
 #endif // LOGIN_H

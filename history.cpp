03c0b76e58371b3e10bfe0c496de457864deb460
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:41:11 +0100

    1st commit from client on history.cpp

--- history.cpp
+++ history.cpp
@@ -4,3 +4,54 @@ history::history()
 {
 
 }
+
+history::history(QString NIC, QString fname, QString lname, QString mail, QString phoneC, QString credit, QString tmp)
+{
+    this->NIC=NIC;
+    this->fname=fname;
+    this->lname=lname;
+    this->mail=mail;
+    this->phoneC=phoneC;
+    this->credit=credit;
+    this->tmp=tmp;
+}
+
+void history::saveclient()
+{
+    QFile file ("C:/Users/Skander/Documents/Client1/history.txt");
+        if (!file.open(QIODevice::WriteOnly|  QIODevice::Append | QIODevice::Text))
+            return;
+
+        QTextStream out(&file);
+        out.setCodec("UTF-8");
+        QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " added on " +sDate << "\n";
+}
+
+void history::saveUpdateclient(QString NIC, QString fname, QString lname, QString mail, QString phoneC, QString credit, QString tmp)
+{
+    QFile file ("C:/Users/Skander/Documents/Client1/history.txt");
+        if (!file.open(QIODevice::WriteOnly|  QIODevice::Append | QIODevice::Text))
+            return;
+
+        QTextStream out(&file);
+        out.setCodec("UTF-8");
+        QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
+        out << "\n""*****************************************Update********************************";
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit;
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " updated on " +sDate << "\n";
+        out << "*****************************************Update********************************" "\n";
+
+}
+
+void history::saveDeleteclient()
+{
+    QFile file ("C:/Users/Skander/Documents/Client1/history.txt");
+        if (!file.open(QIODevice::WriteOnly|  QIODevice::Append | QIODevice::Text))
+            return;
+
+        QTextStream out(&file);
+        out.setCodec("UTF-8");
+        QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " deleted on " +sDate << "\n";
+}

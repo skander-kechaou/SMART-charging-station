58dc4ff84e528682746ee7482f2b6bdf7ec76174
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:40:02 +0100

    1st commit from client on qrwidget.h

--- qrwidget.h
+++ qrwidget.h
@@ -1,11 +1,20 @@
 #ifndef QRWIDGET_H
 #define QRWIDGET_H
 
+#include <QWidget>
+#include <QtSvg/QSvgRenderer>
 
-class qrwidget
-{
+class qrwidget : public QWidget{
+    Q_OBJECT
+private:
+    QString data;
 public:
-    qrwidget();
+    explicit qrwidget(QWidget *parent = 0);
+    void setQRData(QString data);
+    QSize* sz = new QSize(220,220);
+    QColor* fg = new QColor(0,0,0);
+protected:
+    void paintEvent(QPaintEvent *);
 };
 
 #endif // QRWIDGET_H

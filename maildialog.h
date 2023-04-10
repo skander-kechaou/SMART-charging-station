eb9b2b73516785f4215a995e505d04a1f268d6c2
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:35:15 +0100

    1st commit from client on maildialog.h

--- maildialog.h
+++ maildialog.h
@@ -2,6 +2,7 @@
 #define MAILDIALOG_H
 
 #include <QDialog>
+#include "client.h"
 
 namespace Ui {
 class mailDialog;
@@ -13,7 +14,11 @@ class mailDialog : public QDialog
 
 public:
     explicit mailDialog(QWidget *parent = nullptr);
+    void setClient(client c);
     ~mailDialog();
+
+private slots:
+    void on_sendButton_clicked();
 
 private:
     Ui::mailDialog *ui;

4a00948534f1e98222a093dbf9546690726615e7
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:35:03 +0100

    1st commit from client on maildialog.cpp

--- maildialog.cpp
+++ maildialog.cpp
@@ -1,5 +1,13 @@
 #include "maildialog.h"
 #include "ui_maildialog.h"
+#include "smtp.h"
+#include <QSslSocket>
+#include <QtNetwork>
+#include "src/src/SmtpMime"
+#include "src/src/smtpclient.h"
+#include "src/src/emailaddress.h"
+#include "src/src/mimemessage.h"
+#include "src/src/mimetext.h"
 
 mailDialog::mailDialog(QWidget *parent) :
     QDialog(parent),
@@ -8,7 +16,18 @@ mailDialog::mailDialog(QWidget *parent) :
     ui->setupUi(this);
 }
 
+void mailDialog::setClient(client c)
+{
+    ui->recipient->setText(c.getEmail());
+}
+
 mailDialog::~mailDialog()
 {
     delete ui;
+}
+
+void mailDialog::on_sendButton_clicked()
+{
+    smtp *s=new smtp("skander.kechaou.e@gmail.com","sfiprjelyxfpafed","smtp.gmail.com",465);
+    s->sendMail("skander.kechaou.e@gmail.com",ui->recipient->text(),ui->subject->text(),ui->message->toPlainText());
 }

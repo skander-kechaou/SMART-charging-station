e9a79a9b9d7ac54359dc04b2edb0973acf35eddd
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:40:34 +0100

    1st commit from client on smtp.h

--- smtp.h
+++ smtp.h
@@ -1,11 +1,49 @@
 #ifndef SMTP_H
 #define SMTP_H
 
+#include <QtNetwork/QAbstractSocket>
+#include <QtNetwork/QSslSocket>
+#include <QSslSocket>
+#include <QtNetwork>
+#include <QString>
+#include <QTextStream>
+#include <QDebug>
+#include <QtWidgets/QMessageBox>
+#include <QByteArray>
 
-class smtp
+class smtp : public QObject
 {
+    Q_OBJECT
+
 public:
-    smtp();
+    smtp( const QString &user, const QString &pass, const QString &host, int port = 465, int timeout = 30000 );
+    ~smtp();
+    void sendMail( const QString &from, const QString &to, const QString &subject, const QString &body );
+
+signals:
+    void status( const QString &);
+
+private slots:
+    void stateChanged(QAbstractSocket::SocketState socketState);
+    void errorReceived(QAbstractSocket::SocketError socketError);
+    void disconnected();
+    void connected();
+    void readyRead();
+
+private:
+    int timeout;
+    QString message;
+    QTextStream *t;
+    QSslSocket *socket;
+    QString from;
+    QString rcpt;
+    QString response;
+    QString user;
+    QString pass;
+    QString host;
+    int port;
+    enum states{Tls, HandShake ,Auth,User,Pass,Rcpt,Mail,Data,Init,Body,Quit,Close};
+    int state;
 };
 
 #endif // SMTP_H

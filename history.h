63ca5c53f93b1ae159dae5c36afbe9b87be858b5
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-10 16:41:19 +0100

    1st commit from client on history.h

--- history.h
+++ history.h
@@ -1,11 +1,24 @@
 #ifndef HISTORY_H
 #define HISTORY_H
 
+#include<QString>
+#include<QMessageBox>
+#include <QDateTime>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 class history
 {
+    QString NIC, fname, lname, mail, phoneC, credit, tmp;
 public:
+
     history();
+    history(QString NIC, QString fname, QString lname, QString mail, QString phoneC, QString credit, QString tmp);
+    void saveclient();
+    QString loadclients();
+    void saveUpdateclient(QString NIC, QString fname, QString lname, QString mail, QString phoneC, QString credit, QString tmp);
+    void saveDeleteclient();
 };
 
 #endif // HISTORY_H

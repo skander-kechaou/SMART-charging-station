4ab855a37debf2f78f91ab12ee602f1f3499670e
nada <nada.latrach@esprit.tn>
2023-04-11 01:22:11 +0100

    sign_up.h version for validation

--- sign_up.h
+++ sign_up.h
@@ -1,13 +1,19 @@
 #ifndef SIGN_UP_H
 #define SIGN_UP_H
-
-#include <QWidget>
-
+#include<QWidget>
+#include <QDialog>
+#include "Account.h"
 namespace Ui {
 class Sign_up;
 }
+class QCamera;
+class QCameraViewfinder;
+class QCameraImageCapture;
+class QVBoxLayout;
+class QMenu;
+class QAction;
+class Sign_up : public QDialog
 
-class Sign_up : public QWidget
 {
     Q_OBJECT
 
@@ -15,8 +21,25 @@ public:
     explicit Sign_up(QWidget *parent = nullptr);
     ~Sign_up();
 
+private slots:
+
+
+    void on_pushButton_clicked();
+void on_signup_clicked();
+    void on_loginbtn_clicked();
+
 private:
     Ui::Sign_up *ui;
+Sign_up *s;
+QCamera *mCamera;
+  QCameraViewfinder *mQCameraViewfinder;
+   QCameraImageCapture *mQCameraImageCapture;
+  QVBoxLayout   *mLayout;
+  QMenu *mOptionsMenu;
+  QAction *mEncenderAction;
+  QAction *mApAction;
+  QAction *mCaptureAction;
+ Account a;
 };
 
 #endif // SIGN_UP_H

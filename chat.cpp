84e0c9d80a580c01507b1768689bc26da2f861c6
nada <nada.latrach@esprit.tn>
2023-04-11 01:17:49 +0100

    last version of chat.cpp

--- chat.cpp
+++ chat.cpp
@@ -1,14 +1,57 @@
 #include "chat.h"
 #include "ui_chat.h"
-
+#include <QTcpSocket>
+#include <QUdpSocket>
+#include <QTextStream>
+#include<QTextEdit>
+#include <QSqlRecord>
+#include "employeemanagement.h"
 Chat::Chat(QWidget *parent) :
-    QWidget(parent),
+    QDialog(parent),
     ui(new Ui::Chat)
 {
-    ui->setupUi(this);
+
+       ui->setupUi(this);
+       mSocket=new QUdpSocket(this);
+      // mSocket->bind()
+      /* mSocket= new QTcpSocket(this);
+          connect(mSocket,&QTcpSocket::readyRead,[&](){
+              QTextStream T(mSocket);
+             auto text =T.readAll();
+             ui->textEdit->append(text);
+          });
+          EmployeeManagement D(this);
+
+        mSocket->connectToHost(D.hostname(), D.port());*/
 }
 
 Chat::~Chat()
 {
     delete ui;
+}
+
+void Chat::on_sendbutton_clicked()
+{
+  auto  datagrama=ui->msj->text().toLatin1();
+mSocket->writeDatagram(datagrama,QHostAddress::Broadcast,ui->port->value());
+   /* mHostname = ui->hostname_2->text();
+       mPort = ui->port->value();
+       accept();*/
+   /* EmployeeManagement m;
+    Employee em;
+    QString test;
+    QSqlQueryModel* model=em.searchID(idemp);
+    QTextStream T(mSocket);
+    T<< model->record(0).value(2).toString() <<":"<<ui->lineEdit_3->text(); //utilisateur
+    mSocket->flush(); //envoyer le msg au serveur
+    ui->lineEdit_3->clear();
+    QTextStream T(mSocket);
+    T<< "nada"<<":"<<ui->lineEdit_2->text(); //utilisateur
+    mSocket->flush(); //envoyer le msg au serveur
+    ui->lineEdit_2->clear();*/
+}
+
+void Chat::on_cancelbutton_clicked()
+{
+ reject();
 }

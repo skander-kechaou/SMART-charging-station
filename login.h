18ddba3dc3fab7cc0258090221b6954a8c02cddd
nada <nada.latrach@esprit.tn>
2023-04-11 01:29:09 +0100

    validation version

--- login.h
+++ login.h
@@ -2,9 +2,13 @@
 #define LOGIN_H
 #include <QMainWindow>
 #include "employee.h"
+#include "Account.h"
+#include<QWidget>
+#include "arduino.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class Login; }
 QT_END_NAMESPACE
+
 
 class Login : public QMainWindow
 {
@@ -15,17 +19,24 @@ public:
     ~Login();
 
 private slots:
-    void on_pushButton_clicked();
 
-    void on_pushButton_2_clicked();
 
     void on_login_clicked();
 
     void on_label_2_linkActivated(const QString &link);
 
+    void on_sign_up_clicked();
+
+    void on_loginbtn_clicked();
+
+    void on_mdp_oublie_label_linkActivated(const QString &link);
+
 private:
     Ui::Login *ui;
        Login *l;
+Account *a;
+QByteArray data;
+Arduino A;
 
 };
 #endif // LOGIN_H

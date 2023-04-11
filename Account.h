3e79d1bccc680acc549f1651d5949fe3e694ebd6
nada <nada.latrach@esprit.tn>
2023-04-11 01:12:54 +0100

    Account.h modified

--- Account.h
+++ Account.h
@@ -2,7 +2,7 @@
 #define ACCOUNT_H
 #include <QApplication>
 #include <QSqlQuery>
-#include "login.h"
+
 
 class Account
 {
@@ -14,8 +14,12 @@ public:
      Account();
      Account(QString username,QString pwd);
     bool connect(QString uname,QString p);
-   bool reset_pwd(QString uname ,QString old_pwd,QString new_pwd);
-
-
+  // bool reset_pwd(QString uname ,QString old_pwd,QString new_pwd);
+    bool sign_up(QString uname,QString pwd,QString role,QString idemp);
+QStringList liste();
+bool ajouter_image(QString username);
+QByteArray fetch_image(QString uname);
+bool reset_pwd(QString user,QString old_mdp,QString new_mdp);
+bool update_mpd_reset(QString uname,QString code);
 };
 #endif // ACCOUNT_H

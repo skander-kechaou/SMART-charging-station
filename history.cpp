10b35f9a725855f338ac76ba028c43e18d0f11b9
skander-kechaou <skander.kechaou@esprit.tn>
2023-04-17 16:26:48 +0100

    1st commit from Client on history.cpp

--- history.cpp
+++ history.cpp
@@ -25,7 +25,7 @@ void history::saveclient()
         QTextStream out(&file);
         out.setCodec("UTF-8");
         QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
-        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " added on " +sDate << "\n";
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< " ✔️ ADDED on " +sDate << "\n";
 }
 
 void history::saveUpdateclient(QString NIC, QString fname, QString lname, QString mail, QString phoneC, QString credit, QString tmp)
@@ -37,10 +37,7 @@ void history::saveUpdateclient(QString NIC, QString fname, QString lname, QStrin
         QTextStream out(&file);
         out.setCodec("UTF-8");
         QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
-        out << "\n""*****************************************Update********************************";
-        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit;
-        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " updated on " +sDate << "\n";
-        out << "*****************************************Update********************************" "\n";
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< " 🔄 UPDATED on " +sDate << "\n";
 
 }
 
@@ -53,5 +50,5 @@ void history::saveDeleteclient()
         QTextStream out(&file);
         out.setCodec("UTF-8");
         QString sDate = QDateTime::currentDateTime().toString("dddd dd MMMM yyyy hh:mm:ss.zzz");
-        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< "\t" " deleted on " +sDate << "\n";
+        out << "\n User : "+NIC + " " +fname+ " " +lname +" " +mail+" "+phoneC+" "+credit<< " ❌ DELETED on " +sDate << "\n";
 }
